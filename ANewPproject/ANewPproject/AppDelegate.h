76e03ae5426b3beed938c523ee468da6d250d868
Kevin · Martin <1329980100@qq.com>
2015-07-16 11:23:42 +0800

    This is first project

---
+++ ANewPproject/ANewPproject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ANewPproject
+//
+//  Created by WeiFanTech on 15/7/13.
+//  Copyright (c) 2015年 Beijing Weifan technology co., LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ANewPproject/ANewPproject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ANewPproject
+//
+//  Created by WeiFanTech on 15/7/13.
+//  Copyright (c) 2015年 Beijing Weifan technology co., LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
